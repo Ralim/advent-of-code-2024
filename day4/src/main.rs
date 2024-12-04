1540a92450dae6e3969fa807febbdd29493446e2
Ben V. Brown <Ralim@Ralimtek.com>
2024-12-04 16:48:05 +1100

    Remove locking prints

--- day4/src/main.rs
+++ day4/src/main.rs
@@ -14,7 +14,6 @@ fn part_a(path: &str) -> i64 {
 
         Array2D::from_row_major(filtered_file.as_bytes(), lines[0].len(), lines.len()).unwrap()
     };
-    println!("Grid is {} by {}", grid.num_rows(), grid.num_columns());
 
     let search_words = ["XMAS", "SAMX"];
 
@@ -31,7 +30,7 @@ fn part_a(path: &str) -> i64 {
                                 .iter()
                                 .map(|word| {
                                     if search_at(&grid, word, dir, row, col) {
-                                        println!("Found {} at {},{}", word, row, col);
+                                        // println!("Found {} at {},{}", word, row, col);
                                         1
                                     } else {
                                         0
@@ -53,7 +52,6 @@ fn part_b(path: &str) -> i64 {
 
         Array2D::from_row_major(filtered_file.as_bytes(), lines[0].len(), lines.len()).unwrap()
     };
-    println!("Grid is {} by {}", grid.num_rows(), grid.num_columns());
 
     let grid_search = [
         // M_M
